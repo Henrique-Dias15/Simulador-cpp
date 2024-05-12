5f57005c5425b7e0fc90f82186f2739625498721
Henrique Dias <henriquejnd@proton.me>
2024-05-12 18:40:35 -0300

    Adicionado novos personagens

--- principal/main.cpp
+++ principal/main.cpp
@@ -4,6 +4,11 @@
 #include "../acessorios-hpp/Colher.hpp"
 #include "../acessorios-hpp/Escudo.hpp"
 #include "../personagens-hpp/Chaves.hpp"
+#include "../personagens-hpp/Chiquinha.hpp"
+#include "../personagens-hpp/SeuMadruga.hpp"
+#include "../personagens-hpp/ProfessorGirafales.hpp"
+#include "../personagens-hpp/Quico.hpp"
+#include "../personagens-hpp/DonaFlorinda.hpp"
 #include "../core-simulador-hpp/Simulador.hpp"
 
 using std::cout;
@@ -12,16 +17,40 @@ using std::string;
 
 int main()
 {
-    ArmaAtaque *arma = new Rosa("Super Rosa Amarela", 0, 10);
-    ArmaAtaque *arma2 = new Colher("Colher de Pata", 0, 50);
-    ArmaDefesa *escudo = new Escudo("Latão", 1);
+    ArmaAtaque *arma = new Rosa("Rosa", 0, 10);
+    ArmaDefesa *escudo = new Escudo("Escudo", 1);
 
     Personagem *chaves1 = new Chaves(1, "Chaves Eq1", 100, arma, escudo);
     Personagem *chaves2 = new Chaves(2, "Chaves Eq2", 100, arma, escudo);
 
+    Personagem *chiquinha1 = new Chiquinha(1, "Chiquinha Eq1", 100, arma, escudo);
+    Personagem *chiquinha2 = new Chiquinha(2, "Chiquinha Eq2", 100, arma, escudo);
+
+    Personagem *seuMadruga1 = new SeuMadruga(1, "Seu Madruga Eq1", 100, arma, escudo);
+    Personagem *seuMadruga2 = new SeuMadruga(2, "Seu Madruga Eq2", 100, arma, escudo);
+
+    Personagem *professorGirafales1 = new ProfessorGirafales(1, "Professor Girafales Eq1", 100, arma, escudo);
+    Personagem *professorGirafales2 = new ProfessorGirafales(2, "Professor Girafales Eq2", 100, arma, escudo);
+
+    Personagem *quico1 = new Quico(1, "Quico Eq1", 100, arma, escudo);
+    Personagem *quico2 = new Quico(2, "Quico Eq2", 100, arma, escudo);
+
+    Personagem *donaFlorinda1 = new DonaFlorinda(1, "Dona Florinda Eq1", 100, arma, escudo);
+    Personagem *donaFlorinda2 = new DonaFlorinda(2, "Dona Florinda Eq2", 100, arma, escudo);
+
     Simulador *simulador = new Simulador();
     simulador->adicionarPersonagem(chaves1, 1);
     simulador->adicionarPersonagem(chaves2, 2);
+    simulador->adicionarPersonagem(chiquinha1, 1);
+    simulador->adicionarPersonagem(chiquinha2, 2);
+    simulador->adicionarPersonagem(seuMadruga1, 1);
+    simulador->adicionarPersonagem(seuMadruga2, 2);
+    simulador->adicionarPersonagem(professorGirafales1, 1);
+    simulador->adicionarPersonagem(professorGirafales2, 2);
+    simulador->adicionarPersonagem(quico1, 1);
+    simulador->adicionarPersonagem(quico2, 2);
+    simulador->adicionarPersonagem(donaFlorinda1, 1);
+    simulador->adicionarPersonagem(donaFlorinda2, 2);
 
     simulador->iniciarSimulacao();
 
